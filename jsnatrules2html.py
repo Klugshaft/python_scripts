6474509d22623bfad1ad1c5915f31674b4df72de
Calvin Kwok <50604521+Klugshaft@users.noreply.github.com>
2020-10-31 22:41:42 +1100

    Update jsnatrules2html.py

--- jsnatrules2html.py
+++ jsnatrules2html.py
@@ -61,7 +61,7 @@ def decode_fwr_in_json() :
 
 
 
-def ft_nat_fwr(cnt, arr_prefix, natfwrdict):
+def ft_nat_fwr(arr_prefix, natfwrdict):
 
 ## convert show configuration security policy | display json
 
@@ -69,73 +69,65 @@ def ft_nat_fwr(cnt, arr_prefix, natfwrdict):
 
     rset_cnt = len(arr_prefix['security']['nat']['source']['rule-set'])
     #print("there are ", rcnt,  "of rules")
+    
+    for rscnt in range(rset_cnt) :
 
-    ruleset = arr_prefix['security']['nat']['source']['rule-set'][cnt]
-    ruleset_name =  ruleset['name']
-    from_zone = ruleset['from']['zone']
-    to_zone = ruleset['to']['zone']
+        ruleset = arr_prefix['security']['nat']['source']['rule-set'][rscnt]
+        ruleset_name =  ruleset['name']
+        from_zone = ruleset['from']['zone']
+        to_zone = ruleset['to']['zone']
+        ruleset_rule = ruleset['rule']
+        rsr_cnt = len(ruleset_rule)
+        
+        for cntrule in range(rsr_cnt) :
+            nat_rules = {}
 
-    ruleset_rule = ruleset['rule']
-    rsr_cnt = len(ruleset_rule)
+            rs_rule_cnt = ruleset_rule[cntrule]
 
+            rs_rule_name = rs_rule_cnt['name']
+            rs_rule_src = rs_rule_cnt['src-nat-rule-match']['source-address-name']
+            rs_rule_dst = rs_rule_cnt['src-nat-rule-match']['destination-address']
 
+            num_rsr_src = len(rs_rule_src)
+            num_rsr_dst = len(rs_rule_dst)
 
-    for cntrule in range(rsr_cnt) :
-        nat_rules = {}
+            rsr_src_objs = [rs_rule_src[i] for i in range(num_rsr_src)]
+            rsr_dst_objs = [rs_rule_dst[i] for i in range(num_rsr_dst)]
 
-        rs_rule_cnt = ruleset_rule[cntrule]
+            rs_rule_pool_name = rs_rule_cnt['then']['source-nat']['pool']['pool-name']
+            nat_rules['no.'] = rscnt + 1
+            nat_rules['ruleset name'] = ruleset_name
+            nat_rules['from_zone'] = from_zone
+            nat_rules['to_zone'] = to_zone
+            nat_rules['rule name'] = rs_rule_name
+            nat_rules['source'] = rsr_src_objs
+            nat_rules['destination'] = rsr_dst_objs
+            nat_rules['source nat pool'] = rs_rule_pool_name
 
-        rs_rule_name = rs_rule_cnt['name']
-        rs_rule_src = rs_rule_cnt['src-nat-rule-match']['source-address-name']
-        rs_rule_dst = rs_rule_cnt['src-nat-rule-match']['destination-address']
-
-        num_rsr_src = len(rs_rule_src)
-        num_rsr_dst = len(rs_rule_dst)
-
-        rsr_src_objs = [rs_rule_src[i] for i in range(num_rsr_src)]
-        rsr_dst_objs = [rs_rule_dst[i] for i in range(num_rsr_dst)]
-
-        rs_rule_pool_name = rs_rule_cnt['then']['source-nat']['pool']['pool-name']
-
-        nat_rules['ruleset name'] = ruleset_name
-        nat_rules['from_zone'] = from_zone
-        nat_rules['to_zone'] = to_zone
-        nat_rules['rule name'] = rs_rule_name
-        nat_rules['source'] = rsr_src_objs
-        nat_rules['destination'] = rsr_dst_objs
-        nat_rules['source nat pool'] = rs_rule_pool_name
-
-        natfwrlist.append(nat_rules)
+            natfwrlist.append(nat_rules)
 
 
-    return natfwrlist
+        return natfwrlist
 
-def ft_srcnat_pool(cnt, arr_prefix, snatpooldict):
+def ft_srcnat_pool(arr_prefix, snatpooldict):
 
 ## convert show configuration security nat source | display json
 
     srcnatpool = []
 
-    poolcnt = len(arr_prefix['security']['nat']['source']['pool'])
-    #print("there are ", poolcnt,  "of snatpool")
+    for snp in range(poolcnt) :
 
-    snatpool = arr_prefix['security']['nat']['source']['pool'][cnt]
+        snatpool = arr_prefix['security']['nat']['source']['pool'][snp]
 
-    snatpoolname = snatpool['name']
-    snatpooladdrlist = snatpool['address']
-    num_snatpooladdr = len(snatpooladdrlist)
+        snatpoolname = snatpool['name']
+        snatpooladdrlist = snatpool['address']
+        num_snatpooladdr = len(snatpooladdrlist)
+        snatpooladdr_objs = [ snatpooladdrlist[i] for i in range(num_snatpooladdr)]
+        srcnatpooldict = {}
+        srcnatpooldict['pool name'] = snatpoolname
+        srcnatpooldict['address'] = snatpooladdr_objs
 
-    snatpooladdr_objs = [ snatpooladdrlist[i] for i in range(num_snatpooladdr)]
-
-    #for num_snatpool in range(poolcnt) :
-
-    srcnatpooldict = {}
-    srcnatpooldict['pool name'] = snatpoolname
-    srcnatpooldict['address'] = snatpooladdr_objs
-
-    srcnatpool.append(srcnatpooldict)
-
-
+        srcnatpool.append(srcnatpooldict)
 
     return srcnatpool
 
@@ -165,40 +157,19 @@ if __name__ == '__main__':
 
     with open(outputfile,'w') as htmlfile:
         # write header
-
+        
         htmlfile.write('Firewall Policy for {} \n'.format(firewall_name))
-        htmlfile.write('Commited on {} \n'.format(fwrdict['configuration']['@']['junos:commit-localtime']))
+        htmlfile.write('Commited on {} \n'.format(snatruledict['configuration']['@']['junos:commit-localtime']))
 
-        # start extracting SNATPOOL
-        for snp in range(pcnt) :
+        snatpools = ft_srcnat_pool(arr_prefix, snatruledict)
+        snatpool_in_html = json2html.convert( json = snatpools)
+        htmlfile.write(snatpool_in_html)
 
-            snatpools = []
-            snatpool_item = ft_srcnat_pool(snp, arr_prefix, snatruledict)
-            snatpools.append(snatpool_item)
-            snatpooljson = json.dumps(snatpools)
-            snatpool_in_html = json2html.convert( json = snatpooljson )
-            htmlfile.write(snatpool_in_html)
-
-        ## start extracting NAT rules
-        for rscnt in range(rset_cnt) :
-
-            natrsList = []
-            natrsList_item = ft_nat_fwr(rscnt, arr_prefix, snatruledict)
-            natrsList.append(natrsList_item)
-            natrsListjson = json.dumps(natrsList)
-            natrsList_in_html = json2html.convert( json = natrsList )
-            htmlfile.write(natrsList_in_html)
+        natruleList = ft_nat_fwr(arr_prefix, snatruledict)
+        natruleList_in_html = json2html.convert( json = natruleList )
+        htmlfile.write(natruleList_in_html)
+      
 
 
     htmlfile.close()
-
-
-
-
-
-
-
-
-
-
 
